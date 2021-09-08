5adde59bf0153f60eb226b7b94077e0708fd06d8
Bárbara Este <barbara@ibm.com>
2021-09-08 12:14:00 -0300

    feat: adds error validation, loader to FeedComponent

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user.model';
-import { AudioService } from 'src/app/services/audio/audio.service';
-import { UserService } from 'src/app/services/user/user.service';
-import { Observable } from 'rxjs';
-import { PostService } from 'src/app/services/post/post.service';
+import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
+import { User } from 'src/app/models/user.model';
+import { PostService } from 'src/app/services/post/post.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'feed',
@@ -14,18 +13,43 @@ import { Post } from 'src/app/models/post.model';
 export class FeedComponent implements OnInit {
   public user: User;
   public posts: any;
+  public error: boolean = false;
+  public loading: boolean = false;
+
+  private postsSubscription!: Subscription;
 
   constructor(private userService: UserService, private postService: PostService) {
     this.user = this.userService.getUser();
   }
 
-  ngOnInit(): void {
-    this.postService
+  ngOnInit() {
+    this.refresh();
+  }
+
+  /**
+   * Realiza uma chamada para /feed
+   */
+  public refresh() {
+    this.loading = true;
+    this.error = false;
+
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe();
+    }
+
+    this.postsSubscription = this.postService
     .getPosts()
-    .subscribe((response: Post[]) => {
-      console.log(response);
-      this.posts = response;
-    });
+    .subscribe(
+      (response: Post[]) => {
+        console.log(response);
+        this.posts = response;
+        this.loading = false;
+      },
+      (error) => {
+        this.error = true;
+        this.loading = false;
+      }
+    );
   }
 
 }
