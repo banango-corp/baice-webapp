2e8982e6e2a99bd3e871ea259cab61ce999232be
Bárbara Este <barbaraeste01@gmail.com>
2021-10-10 20:34:32 -0300

    refactor: updates karma configuration file

--- karma.conf.js
+++ karma.conf.js
@@ -40,9 +40,9 @@ module.exports = function (config) {
     loggers: [
       { type: 'console' }
     ],
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false, // If true, Karma will start and capture all configured browsers, run tests and then exit with an exit code of 0 or 1 depending on whether all tests passed or any tests failed.
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true, // If true, Karma will start and capture all configured browsers, run tests and then exit with an exit code of 0 or 1 depending on whether all tests passed or any tests failed.
     restartOnFileChange: false,
     failOnFailingTestSuite: false, // Enable or disable failure on failing tests.
   });
